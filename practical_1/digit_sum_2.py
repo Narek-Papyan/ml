34f54fee83f2bb97e4b24e86706745cbd0570685
Narek-Papyan <90937200+Narek-Papyan@users.noreply.github.com>
2021-10-20 23:31:55 +0400

    Delete practical_1 directory

--- practical_1/digit_sum_2.py
+++
@@ -1,6 +0,0 @@
-number = int(input())
-num_1 = number // 10
-num_2 = number % 10
-
-print(num_1 + num_2)
-
--- practical_1/round_number.py
+++
@@ -1,7 +0,0 @@
-num = input()*10
-if num%10 < 5:
-    print num//10
-else:
-    print num//10 + 1
-
-
